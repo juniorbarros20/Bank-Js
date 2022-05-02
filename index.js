cf43b362e3ee4288d753de0d23ebbbc45cc2ba4f
juniorbarros20 <58377610+juniorbarros20@users.noreply.github.com>
2022-05-02 10:36:55 -0300

    Melhorando o projeto

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
-class Cliente {
-    nome;
-    cpf;
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
-class ContaCorrente {
-    agencia;
-    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0; 
+const cliente1 = new Cliente("Alexandre", 12345678908);
+const cliente2 = new Cliente("Joaquim", 98765432101);
 
-    sacar(valor){
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
+const contaCorrenteAlexandre = new ContaCorrente(1001, cliente1);
+contaCorrenteAlexandre.depositar(500);
+const conta2 = new ContaCorrente(1002, cliente2);
 
-    depositar(valor){
-        if(valor <= 0){
-            return
-        }
-        this._saldo += valor;
-    }
-}
+let valor = 200;
+contaCorrenteAlexandre.transferir(valor, conta2);
 
-const cliente1 = new Cliente();
-cliente1.nome = "Alexandre"
-cliente1.cpf = 12345678908;
-
-const cliente2 = new Cliente();
-cliente2.nome = "Joaquim"
-cliente2.cpf = 98765432101;
-
-const contaCorrenteAlexandre = new ContaCorrente();
-contaCorrenteAlexandre.agencia = 1001;
-
-contaCorrenteAlexandre.depositar(100);
-contaCorrenteAlexandre.depositar(100);
-contaCorrenteAlexandre.depositar(100);
-
-const valorSacado = contaCorrenteAlexandre.sacar(50);
-console.log(valorSacado)
-
-console.log(contaCorrenteAlexandre);
+console.log(ContaCorrente.numeroDeContas);
